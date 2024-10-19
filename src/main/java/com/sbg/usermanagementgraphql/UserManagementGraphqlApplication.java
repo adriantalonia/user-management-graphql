7c9447c959ecd7d1e83b84393f89f769454de880
Adrian Talonia <adrian.talonia11@gmail.com>
2024-10-19 11:39:41 -0600

    first commit

---
+++ src/main/java/com/sbg/usermanagementgraphql/UserManagementGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.sbg.usermanagementgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbg/usermanagementgraphql/UserManagementGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbg.usermanagementgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
